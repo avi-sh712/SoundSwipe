1d64e7beb3e8d7fe37a17e13381f68e61d5a53b4
Avi Sharma <151015185+avi-sh712@users.noreply.github.com>
2026-06-25 16:52:04 +0000

    end to end awws dynamo db music app

---
+++ api/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+from .routes_feed import router as feed_router
+from .routes_upload import router as upload_router
+
+app = FastAPI()
+
+app.include_router(feed_router, prefix="/api/feed")
+app.include_router(upload_router, prefix="/api/upload")
